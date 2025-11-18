b377453974c47a87edb74058b08e014bba5ab9b9
Elvis2280 <elvismiranda924g@gmail.com>
2025-11-18 14:02:30 +0900

    Refactor schema.sql: remove unnecessary blank lines for improved readability

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS pt_user (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     first_name VARCHAR(255) NOT NULL,
-    last_name  VARCHAR(255) DEFAULT NULL,
+    last_name VARCHAR(255) DEFAULT NULL,
     password VARCHAR(255) NOT NULL,
     reset_password_token VARCHAR(255) DEFAULT NULL,
     role TINYINT NOT NULL DEFAULT 0 COMMENT '0 = user, 1 = admin',
@@ -10,7 +10,6 @@ CREATE TABLE IF NOT EXISTS pt_user (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
-
 CREATE TABLE IF NOT EXISTS pt_work_report (
     work_report_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
@@ -22,7 +21,6 @@ CREATE TABLE IF NOT EXISTS pt_work_report (
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES pt_user(user_id),
     UNIQUE (user_id, work_date)
 );
-
 CREATE TABLE IF NOT EXISTS pt_work_session (
     work_session_id INT AUTO_INCREMENT PRIMARY KEY,
     work_report_id INT NOT NULL,
@@ -34,7 +32,6 @@ CREATE TABLE IF NOT EXISTS pt_work_session (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT fk_report FOREIGN KEY (work_report_id) REFERENCES pt_work_report(work_report_id)
 );
-
 CREATE TABLE IF NOT EXISTS pt_work_break (
     work_break_id INT AUTO_INCREMENT PRIMARY KEY,
     work_session_id INT NOT NULL,
