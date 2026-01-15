1a508bfdb97d41075d046ae15a3bfc631bd4674b
ShanthalaRa0 <shanthala.enon@gmail.com>
2026-01-15 15:23:35 +0900

    Adding ErrorResponse class

--- src/main/java/co/jp/enon/tms/common/security/dto/ErrorResponse.java
+++ src/main/java/co/jp/enon/tms/common/security/dto/ErrorResponse.java
@@ -1,5 +1,43 @@
 package co.jp.enon.tms.common.security.dto;
 
 public class ErrorResponse extends Response {
+	
+    private String error;
+    private String message;
+    private String path;
+    
+    public ErrorResponse() {}
+
+    public ErrorResponse(String error, String message, String path) {
+        this.setError(error);
+        this.setMessage(message);
+        this.setPath(path);
+    }
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+ 
+    
 
 }
