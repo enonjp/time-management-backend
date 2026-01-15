b1f55ca42529a0a7a792339407e1f3996a3eab58
ShanthalaRa0 <shanthala.enon@gmail.com>
2026-01-15 14:43:14 +0900

    Adding Response class with improvements

--- src/main/java/co/jp/enon/tms/common/security/dto/Response.java
+++ src/main/java/co/jp/enon/tms/common/security/dto/Response.java
@@ -1,6 +1,6 @@
 package co.jp.enon.tms.common.security.dto;
 
-public class Response {
+public abstract class Response {
 
 	private Status status;
 
@@ -16,20 +16,4 @@ public class Response {
         this.status = new Status(status);
     }
 
-    public static ErrorResponse createErrorResponse(Exception ex){
-        ErrorResponse errorResponse = new ErrorResponse();
-        if (ex.getClass().getSimpleName().equals("AccessDeniedException")) {
-	        errorResponse.setStatus(new ErrorStatus(ex.getClass().getSimpleName(), "No permission"));
-        } else {
-	        errorResponse.setStatus(new ErrorStatus(ex.getClass().getSimpleName(), ex.getMessage()));
-        }
-        return errorResponse;
-    }
-
-    public static SuccessResponse createSuccessResponse(Object data){
-        SuccessResponse successResponse = new SuccessResponse();
-        successResponse.setStatus(Status.CODE_OK);
-        successResponse.setResult(data);
-        return successResponse;
-    }
 }
